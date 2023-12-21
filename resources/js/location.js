bf9b3a229e12eeb0f4a26f6e0401d3ebd5c62683
o897 <orapelengm239@gmail.com>
2023-12-21 22:08:12 +0200

    Removed location.js maps

--- resources/js/location.js
+++
@@ -1,7 +0,0 @@
-function myMap() {
-  var mapProp= {
-    center:new google.maps.LatLng(-25.507856, 28.017113),
-    zoom:14,
-  };
-  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-  }
