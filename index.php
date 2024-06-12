6c0cb56726b165857641caf9b8264632d1d9e7d9
o897 <orapelengm239@gmail.com>
2024-06-12 16:18:49 +0200

    update index css

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require_once("includes/CSRF.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BikeTrader</title>
     <link rel="icon" href="public/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="public/styles/index.css">
+    <!-- <link rel="stylesheet" href="public/styles/index.css"> -->
     <link rel="stylesheet" href="public/styles/main.css">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
